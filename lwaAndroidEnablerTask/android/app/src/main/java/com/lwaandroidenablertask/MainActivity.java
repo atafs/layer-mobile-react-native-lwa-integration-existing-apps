1c25374cf3cb5fc8fa740cdcbd16f3023d079d06
Americo Tomas <americotomaswork@gmail.com>
2021-02-22 01:14:05 +0000

    no complilation errors

--- lwaAndroidEnablerTask/android/app/src/main/java/com/lwaandroidenablertask/MainActivity.java
+++ lwaAndroidEnablerTask/android/app/src/main/java/com/lwaandroidenablertask/MainActivity.java
@@ -1,8 +1,21 @@
 package com.lwaandroidenablertask;
 
 import com.facebook.react.ReactActivity;
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.Settings;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import com.facebook.react.*;
+import com.facebook.react.modules.core.DefaultHardwareBackBtnHandler;
+import com.facebook.react.shell.MainReactPackage;
 
 public class MainActivity extends ReactActivity {
+  private final int OVERLAY_PERMISSION_REQ_CODE = 2;  // Choose any value
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
@@ -11,5 +24,18 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "lwaAndroidEnablerTask";
+  }
+
+  @Override    
+  protected void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);        
+      
+      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+        if (!Settings.canDrawOverlays(this)) {
+            Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                                       Uri.parse("package:" + getPackageName()));
+            startActivityForResult(intent, OVERLAY_PERMISSION_REQ_CODE);
+        }
+    }
   }
 }
